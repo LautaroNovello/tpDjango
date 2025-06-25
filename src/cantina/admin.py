66d1e9c22277473a63bed43720eaa522392bf20d
lautaro <Lauty.novello@gmail.com>
2025-06-24 21:13:42 -0300

    Django terminado

--- src/cantina/admin.py
+++ src/cantina/admin.py
@@ -5,6 +5,14 @@ from .models import Categoria, Producto, Venta, DetalleVenta, Empleado
 from django.contrib import admin
 from .models import Categoria, Producto, Venta, DetalleVenta, Empleado
 
+#definimos esta clase que hereda la clase TabularInline
+#que permite mostrar un formulario en linea en el admin, en formato tabla para editar los detalles de venta)
+class DetalleVentaInline(admin.TabularInline):
+    model = DetalleVenta
+    readonly_fields = ('precioHistorico','total',)  # campo solo lectura
+    fields = ('producto', 'cantidad', 'precioHistorico', 'total')  # Hay que incluirlo explicitamente sino, no se muestra
+    extra = 0
+
 @admin.register(Categoria)
 class CategoriaAdmin(admin.ModelAdmin):
     list_display = ('id_categoria', 'nombre')
@@ -18,15 +26,22 @@ class ProductoAdmin(admin.ModelAdmin):
 
 @admin.register(Venta)
 class VentaAdmin(admin.ModelAdmin):
-    list_display = ('id_venta', 'fecha_hora_venta', 'empleado')
+    list_display = ('id_venta', 'fecha_hora_venta', 'empleado', 'total_venta')
     list_filter = ('fecha_hora_venta', 'empleado')
     search_fields = ('empleado__nombre', 'empleado__apellido')
+    inlines = [DetalleVentaInline]
+    def total_venta(self, obj):
+        return obj.total()
+    total_venta.short_description = 'Total Venta'
 
 @admin.register(DetalleVenta)
 class DetalleVentaAdmin(admin.ModelAdmin):
-    list_display = ('id_detalleVenta', 'producto', 'venta', 'cantidad', 'precioHistorico')
+    list_display = ('id_detalleVenta', 'producto', 'venta', 'cantidad', 'precioHistorico', 'total')
     list_filter = ('producto',)
     search_fields = ('producto__nombre',)
+    def total(self, obj):
+        return obj.total
+    total.short_description = 'Total Detalle'
 
 @admin.register(Empleado)
 class EmpleadoAdmin(admin.ModelAdmin):
--- src/cantina/models.py
+++ src/cantina/models.py
@@ -16,6 +16,8 @@ from django.db import models
 class Categoria(models.Model):
     id_categoria = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=255)
+    def __str__(self):
+        return self.nombre
 
 class Producto(models.Model):
     id_producto = models.AutoField(primary_key=True)
@@ -27,6 +29,8 @@ class Producto(models.Model):
         on_delete=models.CASCADE,
         related_name='productos'
     )
+    def __str__(self):
+        return self.nombre
 
 class Venta(models.Model):
     id_venta = models.AutoField(primary_key=True)
@@ -38,11 +42,17 @@ class Venta(models.Model):
         null=True,
         blank=True
     )
+    def __str__(self):
+        return str(self.id_venta)
+    #como total es un atributo, no hay que llamarlo como una funcion, por eso va sin los ()
+    def total(self):
+        return sum(detalle.total for detalle in self.detalles.all())
+    
 
 class DetalleVenta(models.Model):
     id_detalleVenta = models.AutoField(primary_key=True)
     cantidad = models.IntegerField()
-    precioHistorico = models.DecimalField(max_digits=10, decimal_places=2)
+    precioHistorico = models.DecimalField(max_digits=10, decimal_places=2, editable=False)
     producto = models.ForeignKey(
         Producto,
         on_delete=models.CASCADE,
@@ -53,6 +63,20 @@ class DetalleVenta(models.Model):
         on_delete=models.CASCADE,
         related_name='detalles'
     )
+    def __str__(self):
+        return str(self.producto) + " - Cantidad: " + str(self.cantidad)
+    #@property permite mostrar este metodo como atributo
+    @property
+    def total(self):
+        return(self.cantidad * self.precioHistorico)
+    def save(self, *args, **kwargs):
+        # Solo si es un nuevo detalle (aún no guardado en la base) y no tiene precioHistorico asignado:
+        if not self.pk and not self.precioHistorico:
+            # Copio el precio del producto en ese momento a precioHistorico
+            self.precioHistorico = self.producto.precio
+        # Guardo el detalle como normalmente se hace
+        super().save(*args, **kwargs)
+
 
 class Empleado(models.Model):
     id_empleado = models.AutoField(primary_key=True)
@@ -60,3 +84,5 @@ class Empleado(models.Model):
     apellido = models.CharField(max_length=255)
     dni = models.IntegerField()
     fecha_nacimiento = models.DateField()
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
